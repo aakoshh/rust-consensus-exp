6d3b428bfa6da233e33eb931ee04ca621b8bb3d0
Akosh Farkash <aakoshh@gmail.com>
2021-11-23 19:55:34 +0000

    Store headers in vector, hash-to-height in map.

--- src/blockchain/store/block.rs
+++ src/blockchain/store/block.rs
@@ -1,5 +1,8 @@
 use crate::{
-    blockchain::{eras::era1, property::*},
+    blockchain::{
+        eras::{era1, era2},
+        property::*,
+    },
     stm::{abort, StmResult, TVar},
 };
 
@@ -31,62 +34,73 @@ pub trait BlockStore<E: Era> {
     ) -> StmResult<Option<EraInputBlockHeader<E>>>;
 }
 
-struct BlockStore1 {
-    headers: TVar<im::HashMap<era1::BlockHash, era1::BlockHeader>>,
-    height_to_hash: TVar<im::Vector<era1::BlockHash>>,
+/// Calculate the hash of an item before it's stored.
+#[derive(Clone)]
+struct Hashed<T: HasHash<'static>>(T::Hash, T);
+
+impl<T: HasHash<'static>> Hashed<T> {
+    fn new(value: T) -> Hashed<T> {
+        Hashed(value.hash(), value)
+    }
+}
+
+/// Block store for era 1.
+pub struct BlockStore1 {
+    headers: TVar<im::Vector<Hashed<era1::BlockHeader>>>,
+    hash_to_height: TVar<im::HashMap<era1::BlockHash, usize>>,
 }
 
 impl BlockStore1 {
-    fn new(genesis: era1::BlockHeader, blocks: Vec<era1::BlockHeader>) -> BlockStore1 {
-        let mut all = vec![(genesis.hash(), genesis)];
-        all.extend(blocks.into_iter().map(|b| (b.hash(), b)));
+    pub fn new(genesis: era1::BlockHeader, blocks: Vec<era1::BlockHeader>) -> BlockStore1 {
+        let mut headers = im::Vector::new();
+        headers.push_back(Hashed::new(genesis));
+        headers.extend(blocks.into_iter().map(Hashed::new));
 
         // Sanity check that this is a chain.
-        for (p, c) in all.iter().zip(all.iter().skip(1)) {
+        for (p, c) in headers.iter().zip(headers.iter().skip(1)) {
             assert_eq!(p.0, c.1.parent_hash);
         }
 
-        let mut height_to_hash = im::Vector::new();
-        height_to_hash.extend(all.iter().map(|(h, _)| h.clone()));
-
-        let mut headers = im::HashMap::new();
-        for (h, b) in all {
-            headers.insert(h, b);
+        let mut hash_to_height = im::HashMap::new();
+        for (idx, h) in headers.iter().enumerate() {
+            hash_to_height.insert(h.0.clone(), idx);
         }
 
         BlockStore1 {
             headers: TVar::new(headers),
-            height_to_hash: TVar::new(height_to_hash),
+            hash_to_height: TVar::new(hash_to_height),
         }
     }
 }
 
 impl BlockStore<era1::Era1> for BlockStore1 {
     fn max_height(&self) -> StmResult<Height> {
-        self.height_to_hash.read().map(|v| v.len() as Height - 1)
+        self.hash_to_height.read().map(|v| v.len() as Height - 1)
     }
 
     fn add_ranking_block(&self, b: EraRankingBlock<era1::Era1>) -> StmResult<()> {
-        let height_to_hash = self.height_to_hash.read()?;
-        let last_hash = height_to_hash.last().unwrap();
+        let headers = self.headers.read()?;
+        let last_hash = headers.last().unwrap().0.clone();
         let parent_hash = uncross(b.parent_hash());
 
-        if &parent_hash != last_hash {
+        if parent_hash != last_hash {
             abort(StoreError::DoesNotExtendChain)
         } else {
-            let hash = b.hash();
+            let hashed = Hashed::new(b);
 
-            self.headers.update(|h| {
+            self.hash_to_height.update(|h| {
                 let mut c = h.clone();
-                c.insert(hash.clone(), b);
+                c.insert(hashed.0.clone(), headers.len());
                 c
             })?;
 
-            self.height_to_hash.update(|h| {
+            self.headers.update(|h| {
                 let mut c = h.clone();
-                c.push_back(hash);
+                c.push_back(hashed);
                 c
-            })
+            })?;
+
+            Ok(())
         }
     }
 
@@ -94,9 +108,9 @@ impl BlockStore<era1::Era1> for BlockStore1 {
         &self,
         h: Height,
     ) -> StmResult<Option<EraRankingBlock<era1::Era1>>> {
-        match self.height_to_hash.read()?.get(h as usize) {
+        match self.headers.read()?.get(h as usize) {
             None => Ok(None),
-            Some(h) => self.get_ranking_block_by_hash(h),
+            Some(h) => Ok(Some(h.1.clone())),
         }
     }
 
@@ -104,25 +118,30 @@ impl BlockStore<era1::Era1> for BlockStore1 {
         &self,
         h: &EraRankingBlockHash<era1::Era1>,
     ) -> StmResult<Option<EraRankingBlock<era1::Era1>>> {
-        self.headers.read().map(|m| m.get(h).map(|h| h.clone()))
+        match self.hash_to_height.read()?.get(h) {
+            None => Ok(None),
+            Some(h) => self.get_ranking_block_by_height(*h as Height),
+        }
     }
 
     fn remove_ranking_blocks_above_height(&self, h: Height) -> StmResult<()> {
-        let height_to_hash = self.height_to_hash.read()?;
+        let headers = self.headers.read()?;
 
-        if height_to_hash.len() as Height <= h + 1 {
+        if headers.len() as Height <= h + 1 {
             return Ok(());
         }
 
-        let mut height_to_hash = self.height_to_hash.read_clone()?;
-        let removed = height_to_hash.split_off((h + 1) as usize);
-        self.height_to_hash.write(height_to_hash)?;
+        let mut headers = headers.as_ref().clone();
+        let removed = headers.split_off((h + 1) as usize);
+        self.headers.write(headers)?;
 
-        let mut headers = self.headers.read_clone()?;
-        for hash in removed {
-            headers.remove(&hash);
-        }
-        self.headers.write(headers)
+        self.hash_to_height.update(|m| {
+            let mut c = m.clone();
+            for h in removed {
+                c.remove(&h.0);
+            }
+            c
+        })
     }
 
     fn add_input_block_header(&self, h: EraInputBlockHeader<era1::Era1>) -> StmResult<()> {
@@ -131,7 +150,7 @@ impl BlockStore<era1::Era1> for BlockStore1 {
     }
 
     fn has_input_block_header(&self, h: &EraInputBlockHash<era1::Era1>) -> StmResult<bool> {
-        self.headers.read().map(|m| m.contains_key(h))
+        self.hash_to_height.read().map(|m| m.contains_key(h))
     }
 
     fn get_input_block_header_by_hash(
@@ -142,3 +161,20 @@ impl BlockStore<era1::Era1> for BlockStore1 {
         self.get_ranking_block_by_hash(h)
     }
 }
+
+/// Block store for era 2.
+pub struct BlockStore2 {
+    rankings: TVar<im::HashMap<era2::RankingBlockHash, era2::RankingBlock>>,
+    inputs: TVar<im::HashMap<era2::InputBlockHash, era2::InputBlockHeader>>,
+    height_to_hash: TVar<im::Vector<era2::RankingBlockHash>>,
+    min_height: TVar<u64>,
+}
+
+// impl BlockStore2 {
+//     pub fn new(
+//         rankings: Vec<era2::RankingBlock>,
+//         inputs: Vec<era2::InputBlockHeader>,
+//     ) -> BlockStore2 {
+
+//     }
+// }
